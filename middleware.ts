4af82267b7bbf11cc059071a7997eb9bb0a5becd
aymen-igr <aymenigri@gmail.com>
2025-12-03 20:59:16 +0100

    adding the project structure of the frontend side

---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    
+  ]
+}
