842e4b4c0e143f51465eb3dbf749eabc9c4a1639
Aymen Igri <aymenigri@gmail.com>
2026-01-03 04:20:59 +0100

    Adding the offices page

---
+++ app/superAdmin/offices/Components/selectedOffice.tsx
@@ -0,0 +1,86 @@
+"use client";
+
+import { User } from "lucide-react";
+import { X } from "lucide-react";
+import { useState } from "react";
+
+export default function SelectedOffice({
+  selectedOffice,
+  setSelectedOffice,
+}:{
+  selectedOffice: any,
+  setSelectedOffice: any
+}) {
+
+  const [loading, setLoading] = useState(false);
+  
+  return (
+    <div
+      className="fixed inset-0 bg-black/90 bg-opacity-10 flex items-center justify-center z-50 text-white"
+      onClick={() => setSelectedOffice(null)}
+    >
+      <div
+        className="bg-[#4d0000] rounded-lg p-8 max-w-2xl w-full h-[90%] mx-4 relative overflow-y-auto"
+        onClick={(e) => e.stopPropagation()}
+      >
+        <button
+          onClick={() => setSelectedOffice(null)}
+          className="absolute top-4 right-4 text-gray-400 hover:text-white"
+        >
+          <X size={24} />
+        </button>
+
+        <div className="flex flex-col items-center gap-6">
+          <div className="flex flex-row justify-start items-center w-full gap-4">
+            <div className="w-14 h-14 bg-[#7F0000] rounded-full flex items-center justify-center">
+              <User size={30} className="text-white" />
+            </div>
+            <div>
+                <h2 className="text-xl font-bold text-white">
+              {selectedOffice.name}
+            </h2>
+            </div>
+            
+          </div>
+
+          <div className="w-full space-y-4 text-sm">
+            <div className="flex justify-between border-b border-gray-700 pb-2">
+              <span className="text-gray-200">ID:</span>
+              <span className="text-white font-semibold">
+                {selectedOffice.id}
+              </span>
+            </div>
+            <div className="flex justify-between border-b border-gray-700 pb-2">
+              <span className="text-gray-200">Specility:</span>
+              <span className="text-white">{selectedOffice.specility}</span>
+            </div>
+            <div className="flex justify-between border-b border-gray-700 pb-2">
+              <span className="text-gray-200">Phone:</span>
+              <span className="text-white">{selectedOffice.phone}</span>
+            </div>
+            <div className="flex justify-between border-b border-gray-700 pb-2">
+              <span className="text-gray-200">Default consultation price:</span>
+              <span className="text-white">{selectedOffice.defaultConsultPrice}</span>
+            </div>
+            <div className="flex justify-between border-b border-gray-700 pb-2">
+              <span className="text-gray-200">Founder:</span>
+              <span className="text-white">{selectedOffice.createdBy}</span>
+            </div>
+            <div className="flex justify-between border-b border-gray-700 pb-2">
+              <span className="text-gray-200">Creation Date:</span>
+              <span className="text-white">{selectedOffice.createdAt}</span>
+            </div>
+            <div className="flex justify-between border-b border-gray-700 pb-2">
+              <span className="text-gray-200">Status:</span>
+              <span className="text-white">{selectedOffice.status}</span>
+            </div>
+            <div className="flex justify-between border-b border-gray-700 pb-2">
+              <span className="text-gray-200">Description:</span>
+              <span className="text-white">{selectedOffice.description}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/superAdmin/offices/page.tsx
+++ app/superAdmin/offices/page.tsx
@@ -1,7 +1,106 @@
-export default function OfficesPage() {
-    return(
-        <div>
-            Offices Page
+"use client";
+
+import { useState, useEffect } from "react";
+import OfficesList from "./Components/officesList";
+import SelectedOffice from "./Components/selectedOffice";
+
+
+export default function UsersPage() {
+  const [selectedOffice, setSelectedOffice] = useState(null);
+  const [offices, setOffices] = useState<any[]>([]);
+  const [searchType, setSearchType] = useState<string>("name");
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  useEffect(() => {
+    try{
+      const response = fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setOffices(data);
+      })
+      .catch((error) => {
+        throw new Error("Error fetching users:", error);
+      });
+    }catch(error){
+      console.error(error);
+    }  
+  }, []);
+
+  const filterOffices = offices.filter((office) => {
+    if (!searchQuery) return true;
+    const query = searchQuery.toLowerCase();
+
+    switch (searchType) {
+      case "name":
+        return office.name?.toLowerCase().includes(query);
+      case "email":
+        return office.phone?.toLowerCase().includes(query);
+      case "id":
+        return office.id?.toString().includes(query);
+      case "status":
+        return office.status?.toLowerCase().includes(query);
+      default:
+        return true;
+    }
+  });
+
+  return (
+    <div className="w-full px-6 py-8 mt-3">
+      <div className="mb-6 flex items-center gap-4">
+        <div className="flex-1 max-w-lg">
+          <div className="flex gap-2">
+            <select
+              id="searchType"
+              value={searchType}
+              onChange={(e) => setSearchType(e.target.value)}
+              className="h-10 px-4 py-2 bg-[#4D0000] text-white rounded-lg border border-[#3d0000] focus:outline-none focus:ring-2 focus:ring-[#7F0000] focus:border-transparent cursor-pointer hover:bg-[#7F0000] transition-colors"
+            >
+              <option value="name">Name</option>
+              <option value="email">Email</option>
+              <option value="id">ID</option>
+              <option value="gender">Gender</option>
+            </select>
+            <input
+              id="search"
+              type="text"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder={`Search by ${searchType}...`}
+              className="h-10 flex-1 px-4 py-2 bg-[#4D0000] text-white rounded-lg border border-[#3d0000] focus:outline-none focus:ring-2 focus:ring-[#7F0000] focus:border-transparent placeholder-gray-500"
+            />
+          </div>
         </div>
-    )
+      </div>
+
+      <div className="flex gap-8 flex-1 ml-19">
+        <div className="w-24">
+          <p className="text-white font-semibold">ID</p>
+        </div>
+        <div className="w-30">
+          <p className="text-white font-semibold">Name</p>
+        </div>
+        <div className="w-42">
+          <p className="text-white font-semibold">Phone</p>
+        </div>
+        <div className="w-32">
+          <p className="text-white font-semibold">Status</p>
+        </div>
+        <div className="w-72">
+          <p className="text-white font-semibold">Founder</p>
+        </div>
+        <div className="w-24">
+          <p className="text-white font-semibold">Creation date</p>
+        </div>
+      </div>
+      <OfficesList setSelectedOffice={setSelectedOffice} offices={filterOffices} />
+      {selectedOffice && (
+        <SelectedOffice selectedOffice={selectedOffice} setSelectedOffice={setSelectedOffice} />
+      )}
+    </div>
+  );
 }
